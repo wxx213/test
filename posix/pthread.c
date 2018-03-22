e4d84a987febadc366115ccdc078941c46055d61
wxx <stellarwxx@163.com>
2018-03-22 23:06:22 +0800

    update pthread codes
    1. correct printf format for long unsigned int
    2. test thread local variates share among threads

--- posix/pthread.c
+++ posix/pthread.c
@@ -4,39 +4,49 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-void thread1(void)
+void *thread1(void *x)
 {
+	int local = 2;
+
 	printf("thread1 pid: %d\n",getpid());
-	printf("thread1 tid: %d\n",pthread_self());
+	printf("thread1 tid: %lu\n",pthread_self());
+	printf("thread1 local: %d\n", local);
 	pthread_exit(NULL);
+	return NULL;
 }
-void thread2(void)
+void *thread2(void *x)
 {
+	int local = 3;
+
 	printf("thread2 pid: %d\n",getpid());
-	printf("thread1 tid: %d\n",pthread_self());
+	printf("thread2 tid: %lu\n",pthread_self());
+	printf("thread2 local: %d\n", local);
 	pthread_exit(NULL);
+	return NULL;
 }
 int main()
 {
-	pthread_t pid1,pid2;
+	pthread_t tid1,tid2;
 	int retval;
+	int local = 1;
 	
-	retval = pthread_create(&pid1,NULL,(void *)thread1,NULL);
+	retval = pthread_create(&tid1,NULL,&thread1,NULL);
 	if(retval != 0)
 	{
 		printf("error1\n");
 		return 0;
 	}
-	retval = pthread_create(&pid2,NULL,(void *)thread2,NULL);
+	retval = pthread_create(&tid2,NULL,&thread2,NULL);
 	if(retval != 0)
 	{
 		printf("error2\n");
 		return 0;
 	}
 	printf("main pid: %d\n",getpid());
-	printf("main pid1: %d\n",pid1);
-	printf("main pid2: %d\n",pid2);
-	pthread_join(pid1,NULL);
-	pthread_join(pid2,NULL);
+	printf("main tid1: %lu\n",tid1);
+	printf("main tid2: %lu\n",tid2);
+	printf("main local: %d\n", local);
+	pthread_join(tid1,NULL);
+	pthread_join(tid2,NULL);
 	return 0;
 }
