2c5cb46378d33e74299b50c6d0de3be45944f547
Xingxing Wang <stellarwxx@163.com>
2019-11-30 00:18:56 +0800

    golang: process: add to test Channel
    Signed-off-by: Xingxing Wang <stellarwxx@163.com>

--- golang/process/main.go
+++ golang/process/main.go
@@ -2,10 +2,22 @@ package main
 
 import (
 	"fmt"
+	"flag"
 )
 
+var test string
+func init() {
+	flag.StringVar(&test, "t", "r", "test process to run")
+}
+
 func main() {
+	flag.Parse()
 	fmt.Println("starting test process")
-	// testRoutine()
-	testProcess()
+	if test == "r" {
+		testRoutine()
+	} else if test == "p" {
+		testProcess()
+	} else {
+		fmt.Println("nothing to do")
+	}
 }
--- golang/process/routine.go
+++ golang/process/routine.go
@@ -1,23 +1,27 @@
 package main
 
 import (
-        "fmt"
-        "sync"
+	"fmt"
+	"sync"
 )
 
 var wg sync.WaitGroup
+var myCh chan int
 
 func routine1() {
         fmt.Println("runing routine1")
+		myCh <- 2
         wg.Done()
 }
 
 func routine2() {
         fmt.Println("runing routine2")
+		fmt.Println("received channel data:", <-myCh)
         wg.Done()
 }
 
 func testRoutine() {
+		myCh = make(chan int, 3)
         go routine1()
         go routine2()
         wg.Add(2)
