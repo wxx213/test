eb4231bf496def79df3906734ea0d229dede99ea
Xingxing Wang <stellarwxx@163.com>
2019-12-01 15:00:07 +0800

    golang: process: add to test channel traverse

--- golang/process/routine.go
+++ golang/process/routine.go
@@ -7,24 +7,52 @@ import (
 
 var wg sync.WaitGroup
 var myCh chan int
+var myCh2 chan int
 
 func routine1() {
-        fmt.Println("runing routine1")
-		myCh <- 2
-        wg.Done()
+	fmt.Println("runing routine1")
+	myCh <- 0
+	myCh <- 1
+	myCh <- 2
+	// close channel for traverse
+	close(myCh)
+	myCh2 <- 3
+	wg.Done()
 }
 
 func routine2() {
-        fmt.Println("runing routine2")
-		fmt.Println("received channel data:", <-myCh)
-        wg.Done()
+	fmt.Println("runing routine2")
+	select {
+		case data := <-myCh:
+			fmt.Println("received channel data:", data)
+			for {
+				if v,ok := <-myCh; ok {
+					fmt.Println(v)
+					fmt.Println(ok)
+				} else {
+					break
+				}
+			}
+		case data2 := <-myCh2:
+			fmt.Println("received channel2 data:", data2)
+	}
+
+	select {
+		case data := <-myCh:
+			fmt.Println("received channel data:", data)
+		case data2 := <-myCh2:
+			fmt.Println("received channel2 data:", data2)
+	}
+
+	wg.Done()
 }
 
 func testRoutine() {
-		myCh = make(chan int, 3)
-        go routine1()
-        go routine2()
-        wg.Add(2)
-        wg.Wait()
+	myCh = make(chan int, 3)
+	myCh2 = make(chan int, 3)
+	go routine1()
+	go routine2()
+	wg.Add(2)
+	wg.Wait()
 }
 
