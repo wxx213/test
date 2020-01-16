64413eed85edeffdb220a183b33043fa29de6383
Xingxing Wang <stellarwxx@163.com>
2020-01-16 23:30:54 +0800

    java: zookeeper: make zookeeper startup in docker image

---
+++ java/zookeeper/zookeeper.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+/bin/bash /usr/local/zookeeper/apache-zookeeper-3.5.6-bin/bin/zkServer-initialize.sh
+/bin/bash /usr/local/zookeeper/apache-zookeeper-3.5.6-bin/bin/zkServer.sh start
+/bin/bash
