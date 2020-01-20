1bf863fdb0790d10a48f3332f847d3882dac180c
Xingxing Wang <stellarwxx@163.com>
2020-01-20 21:49:10 +0800

    java: redis: add redis cluster support for docker image

--- java/redis/redis.sh
+++ java/redis/redis.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-/usr/local/redis/redis-5.0.7/src/redis-server &
-/bin/sleep 3
-/usr/local/redis/redis-5.0.7/src/redis-cli CONFIG SET protected-mode no
+/usr/local/redis/redis-5.0.7/src/redis-server /usr/local/redis/redis-5.0.7/redis_cluster.conf &
+
 /bin/bash
