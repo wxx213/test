b5093783eba7258da8b555ec4897268f70bdb10f
Xingxing Wang <stellarwxx@163.com>
2020-01-24 20:44:51 +0800

    golang: etcd: deploy etcd node with discovery url for cluster setup

--- golang/etcd/etcd.sh
+++ golang/etcd/etcd.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
-/usr/local/etcd/etcd-v3.4.2-linux-amd64/etcd &
+#
+# get the discovery url by:
+# curl https://discovery.etcd.io/new?size=3
+# https://discovery.etcd.io/dbc474e91817301dfafc4d7876a926c1
+#
+
+HOST_IP=`/sbin/ifconfig eth0 | awk '/inet/ {print $2}' | cut -f2 -d ":" |awk 'NR==1 {print $1}'`
+ETCD_NAME="etcd_${HOST_IP}_ip"
+DISCOVERY_URL="https://discovery.etcd.io/dbc474e91817301dfafc4d7876a926c1"
+
+/usr/local/etcd/etcd-v3.4.2-linux-amd64/etcd --name ${ETCD_NAME} --initial-advertise-peer-urls http://${HOST_IP}:2380 \
+--listen-peer-urls http://${HOST_IP}:2380 \
+--listen-client-urls http://${HOST_IP}:2379,http://127.0.0.1:2379 \
+--advertise-client-urls http://${HOST_IP}:2379 \
+--discovery $DISCOVERY_URL &
 
 /bin/bash
