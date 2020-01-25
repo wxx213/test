48efb9c486df0cc937ce156a6650dd654b6ea98f
Xingxing Wang <stellarwxx@163.com>
2020-01-26 00:07:54 +0800

    golang: etcd: add initial dns discovery deploy
    just initial, the dns discovery still cannot work normally.

--- golang/etcd/deploy_etcd_cluster.sh
+++ golang/etcd/deploy_etcd_cluster.sh
@@ -1,15 +1,31 @@
 #!/bin/bash
 
 NODE_NUM=$1
+DISCOVERY_TYPE=$2
 
-DISCOVERY_URL=`curl https://discovery.etcd.io/new?size=$NODE_NUM`
+if [ $DISCOVERY_TYPE == "etcd" ]
+then
+	DISCOVERY_URL=`curl https://discovery.etcd.io/new?size=$NODE_NUM`
 
-for((i=1;i<=$NODE_NUM;i++));
-do
-	docker run -itd --name ed$i --env ETCD_DISCOVERY_URL=$DISCOVERY_URL etcd
-	if [ $? != 0 ]
-	then
-		exit 1
-	fi
-done
+	for((i=1;i<=$NODE_NUM;i++));
+	do
+		docker run -itd --name ed$i --env ETCD_DISCOVERY_TYPE=etcd --env ETCD_DISCOVERY_URL=$DISCOVERY_URL etcd
+		if [ $? != 0 ]
+		then
+			exit 1
+		fi
+	done
+elif [ $DISCOVERY_TYPE == "dns" ]
+then
+	DISCOVERY_SRV="wxx.example.com"
+
+	for((i=1;i<=$NODE_NUM;i++));
+	do
+		docker run -itd --name ed$i --env ETCD_DISCOVERY_TYPE=dns --env WXX_ETCD_DISCOVERY_SRV=$DISCOVERY_SRV etcd
+		if [ $? != 0 ]
+		then
+			exit 1
+		fi
+	done
+fi
 exit 0
--- golang/etcd/etcd.sh
+++ golang/etcd/etcd.sh
@@ -8,12 +8,29 @@
 
 HOST_IP=`/sbin/ifconfig eth0 | awk '/inet/ {print $2}' | cut -f2 -d ":" |awk 'NR==1 {print $1}'`
 ETCD_NAME="etcd_${HOST_IP}_ip"
-DISCOVERY_URL=$ETCD_DISCOVERY_URL
 
-/usr/local/etcd/etcd-v3.4.2-linux-amd64/etcd --name ${ETCD_NAME} --initial-advertise-peer-urls http://${HOST_IP}:2380 \
---listen-peer-urls http://${HOST_IP}:2380 \
---listen-client-urls http://${HOST_IP}:2379,http://127.0.0.1:2379 \
---advertise-client-urls http://${HOST_IP}:2379 \
---discovery $DISCOVERY_URL &
+if [ $ETCD_DISCOVERY_TYPE == "etcd" ]
+then
+	DISCOVERY_URL=$ETCD_DISCOVERY_URL
+
+	/usr/local/etcd/etcd-v3.4.2-linux-amd64/etcd --name ${ETCD_NAME} --initial-advertise-peer-urls http://${HOST_IP}:2380 \
+	--listen-peer-urls http://${HOST_IP}:2380 \
+	--listen-client-urls http://${HOST_IP}:2379,http://127.0.0.1:2379 \
+	--advertise-client-urls http://${HOST_IP}:2379 \
+	--discovery $DISCOVERY_URL &
+elif [ $ETCD_DISCOVERY_TYPE == "dns" ]
+then
+	DISCOVERY_SRV=$WXX_ETCD_DISCOVERY_SRV
+	CLUSTER_TOKEN="etcd-cluster"
+
+	/usr/local/etcd/etcd-v3.4.2-linux-amd64/etcd --name ${ETCD_NAME} \
+	--discovery-srv $DISCOVERY_SRV \
+	--initial-advertise-peer-urls http://${ETCD_NAME}.${DISCOVERY_SRV}:2380 \
+	--initial-cluster-token $CLUSTER_TOKEN \
+	--initial-cluster-state new \
+	--advertise-client-urls http://${ETCD_NAME}.${DISCOVERY_SRV}:2379 \
+	--listen-client-urls http://0.0.0.0:2379 \
+	--listen-peer-urls http://0.0.0.0:2380
+fi
 
 /bin/bash
