ee7e0e7f4a4b72db3bc6a22fe90a9130fc0b0c88
wxx123 <stellarwxx@163.com>
2020-12-27 23:56:40 +0800

    java: add dubbo spi test code

--- java/spi-demo/spi-service/src/main/java/OrderService.java
+++ java/spi-demo/spi-service/src/main/java/OrderService.java
@@ -1,5 +1,9 @@
 package com.demo.spi.service;
 
+import org.apache.dubbo.common.extension.SPI;
+
+// only dubbo need this annotation
+@SPI
 public interface OrderService {
     int getOrderCountById(int id);
 }
--- java/spi-demo/spi-test/src/main/java/com/spi/test/TestSpi.java
+++ java/spi-demo/spi-test/src/main/java/com/spi/test/TestSpi.java
@@ -3,13 +3,27 @@ package com.spi.test;
 import java.util.ServiceLoader;
 
 import com.demo.spi.service.OrderService;
+import org.apache.dubbo.common.extension.ExtensionLoader;
 
 public class TestSpi {
-    public static void main(String[] args) {
+    public static void testJavaSPI() {
+        System.out.println("Java Spi Test");
         ServiceLoader<OrderService> orderServices = ServiceLoader.load(OrderService.class);
         for (OrderService orderService : orderServices) {
             orderService.getOrderCountById(1);
         }
+    }
+    public static void testDubboSPI() {
+        System.out.println("Dubbo Spi Test");
+        ExtensionLoader<OrderService> extensionLoader = ExtensionLoader.getExtensionLoader(OrderService.class);
+        OrderService agencyService = extensionLoader.getExtension("agencyOrder");
+        agencyService.getOrderCountById(1);
+        OrderService customerService = extensionLoader.getExtension("customerOrder");
+        customerService.getOrderCountById(1);
+    }
+    public static void main(String[] args) {
+        testJavaSPI();
+        testDubboSPI();
     }
 
 }
