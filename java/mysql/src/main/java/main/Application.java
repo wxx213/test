8514014baf5adf31e4b8de9da587d45cc7cb9942
wxx123 <stellarwxx@163.com>
2021-07-27 00:36:25 +0800

    java: mysql: add mybatis generator demo

--- java/mysql/src/main/java/main/Application.java
+++ java/mysql/src/main/java/main/Application.java
@@ -2,10 +2,16 @@ package main;
 
 import basic.JdbcTest;
 import mybatis.basic.MybatisTest;
+import mybatis.generator.Generator;
 
 public class Application {
     public static void main(String[] args) {
         // JdbcTest.test(args);
-        MybatisTest.startMybatisTest(args);
+        // MybatisTest.startMybatisTest(args);
+        try {
+            new Generator().testGenerator(args);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
